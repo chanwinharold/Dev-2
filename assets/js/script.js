c78db288905bb931af4b65e7767a11464e6bf0e5
chanwinharold <153543115+chanwinharold@users.noreply.github.com>
2024-10-27 21:04:25 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/*let myName = "harold";
+let age = 17;
+const origin = "Benin";
+const gender = "boy";
+
+console.log(`hello! It's ${myName} and I'm ${age}. I'm from ${origin} and I'm a ${gender}.`);
+console.log("Location:", origin.toUpperCase());*/
